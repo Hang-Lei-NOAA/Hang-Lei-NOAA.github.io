d8dea9573cf4a5460618a6c5d217b3bd58a8eec9
Hang.Lei <Hang.Lei@noaa.gov>
2021-07-12 22:42:46 +0000

    part of g2 docs

---
+++ NCEPLIBS/NCEPLIBS-g2/html/search/functions_67.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['getdrsindex',['getdrsindex',['../classdrstemplates.html#ac89d817b09f258853954a0e5009e26fa',1,'drstemplates']]],
+  ['getdrstemplate',['getdrstemplate',['../classdrstemplates.html#a289f2f4b832edac2286b1d494df0fc5d',1,'drstemplates']]]
+];
